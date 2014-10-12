f835df8fd144b30074d880fcf001c0d42000684b
raul <raul@raul-VAIO>
2014-10-12 21:33:35 +0200

    Vuelta al ejercicio y clases original para realizar un buen diseño, utilizando el polimorfismo

--- src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/Addition.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/Addition.java
@@ -1,9 +1,9 @@
 package es.upm.miw.pd.doo.polymorphism.colecciones.operaciones;
 
-public class Multiplication {
+public class Addition {
     private final int operator1, operator2;
 
-    public Multiplication(final int operator1, final int operator2) {
+    public Addition(final int operator1, final int operator2) {
         this.operator1 = operator1;
         this.operator2 = operator2;
     }
@@ -17,12 +17,12 @@ public class Multiplication {
     }
 
     public int sum() {
-        return this.operator1 * this.operator2;
+        return this.operator1 + this.operator2;
     }
 
     @Override
     public String toString() {
-        return "[" + operator1 + "*" + operator2 + "]";
+        return "[" + operator1 + "+" + operator2 + "]";
     }
 
 }
