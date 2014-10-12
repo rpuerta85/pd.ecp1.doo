c09936b55cd85d864b457c1d170f272d1390e3a3
raul <raul@raul-VAIO>
2014-10-12 22:57:10 +0200

    Añadida clase manejadira de Operaciones "OperationHandler"

---
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/OperationHandler.java
@@ -0,0 +1,24 @@
+package es.upm.miw.pd.doo.polymorphism.colecciones.operaciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperationHandler {
+
+  private List<Operations> operations = new ArrayList<Operations>();
+  
+  public void add(Operations operation) {
+      operations.add(operation);
+  }
+
+  public void reset() {
+     operations.clear();
+  }
+  public int total() {
+      int result=0;
+      for (int i=0;i<operations.size();i++) {
+          result+=operations.operar();
+      }
+     return result;
+  }
+}
