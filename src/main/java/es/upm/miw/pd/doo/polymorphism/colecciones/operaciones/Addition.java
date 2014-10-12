3c04bd49b8634862d66f0f83f5cb734a126bfddc
raul <raul@raul-VAIO>
2014-10-12 23:40:31 +0200

    Refactorizada clase "Addition"

--- src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/Addition.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/Addition.java
@@ -1,28 +1,15 @@
 package es.upm.miw.pd.doo.polymorphism.colecciones.operaciones;
 
-public class Addition {
-    private final int operator1, operator2;
+public class Addition extends Operations {
+
+    private static final char OPERATOR = '+';
 
     public Addition(final int operator1, final int operator2) {
-        this.operator1 = operator1;
-        this.operator2 = operator2;
-    }
-
-    public int getOperator1() {
-        return operator1;
-    }
-
-    public int getOperator2() {
-        return operator2;
-    }
-
-    public int sum() {
-        return this.operator1 + this.operator2;
+        super(operator1, operator2, OPERATOR);
     }
 
     @Override
-    public String toString() {
-        return "[" + operator1 + "+" + operator2 + "]";
+    protected int operar() {
+        return this.operator1 + this.operator2;
     }
-
 }
--- src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/Operations.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/colecciones/operaciones/Operations.java
@@ -2,15 +2,24 @@ package es.upm.miw.pd.doo.polymorphism.colecciones.operaciones;
 
 public abstract class Operations {
     
-    private int operator1;
-    private int operator2;
-    private char operator;
-    
-    public Operations(int operator1,int operator2,char operator){
-        this.operator1=operator1;
-        this.operator2=operator2;
-        this.operator=operator;
+    protected int operator1;
+    protected int operator2;
+    protected char operator;
+
+    public Operations(int operator1,int operator2,char operator) {
+        this.operator1 = operator1;
+        this.operator2 = operator2;
+        this.operator = operator;
     }
-    
+
+    public void setOperator1(int operator1) {
+        this.operator1 = operator1;
+    }
+
     protected abstract int operar();
+
+    @Override
+    public String toString() {
+        return "[" + operator1 + operator + operator2 + "]";
+    }
 }
