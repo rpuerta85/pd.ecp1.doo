f0680d75a06e4bb766f643deab26dd4a28c47331
raul <raul@raul-VAIO>
2014-10-21 21:30:53 +0200

    Añadida interfaz pública de clase para la clase "GestorVehiculos"

--- src/main/java/es/upm/miw/pd/doo/polymorphism/vehiculos/models/GestorVehiculos.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/vehiculos/models/GestorVehiculos.java
@@ -1,7 +1,11 @@
 package es.upm.miw.pd.doo.polymorphism.vehiculos.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorVehiculos {
     private static  GestorVehiculos singleton = null;
+    private List<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
     
     private GestorVehiculos() {
         
@@ -12,6 +16,26 @@ public class GestorVehiculos {
             singleton = new GestorVehiculos();
         }
     return singleton;
+    }
+
+    public void insertar(Vehiculo coche) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public Vehiculo buscar(Vehiculo coche) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Vehiculo actualizar(Vehiculo coche) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void eliminar(Vehiculo coche) {
+        // TODO Auto-generated method stub
+        
     }
 
 
