5d607f6bb21a8facce7bc10dd18eb09ca5c4b376
raul <raul@raul-VAIO>
2014-10-21 20:58:56 +0200

    Añadida interfaz públcia de clase "Coche"

---
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/vehiculos/models/Coche.java
@@ -0,0 +1,12 @@
+package es.upm.miw.pd.doo.polymorphism.vehiculos.models;
+
+public class Coche extends Vehiculo {
+    
+    
+    public Coche(int id, String descripcion) {
+      super(id,descripcion);
+    }
+
+ 
+
+}
