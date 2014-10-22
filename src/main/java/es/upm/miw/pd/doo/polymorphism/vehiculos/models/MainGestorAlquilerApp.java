e537faa257925be91e14ca5634580a823db8d9b6
raul <raul@raul-VAIO>
2014-10-22 19:50:58 +0200

    Añadido receptor del patron comando junto con us interfaz publica de clase

---
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/vehiculos/models/MainGestorAlquilerApp.java
@@ -0,0 +1,10 @@
+package es.upm.miw.pd.doo.polymorphism.vehiculos.models;
+
+public class MainGestorAlquilerApp {
+
+    public void alquilar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
