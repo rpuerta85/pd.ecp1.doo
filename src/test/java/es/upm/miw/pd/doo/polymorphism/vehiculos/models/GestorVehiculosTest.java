ab110c973958d8968581d84ae71bf6df55959854
raul <raul@raul-VAIO>
2014-10-22 22:46:36 +0200

    Corregido test "GestorVehiculos"

--- src/test/java/es/upm/miw/pd/doo/polymorphism/vehiculos/models/GestorVehiculosTest.java
+++ src/test/java/es/upm/miw/pd/doo/polymorphism/vehiculos/models/GestorVehiculosTest.java
@@ -16,7 +16,7 @@ public class GestorVehiculosTest {
     @Before
     public void ini() {
         gestorVehiculos = GestorVehiculos.getGestorVehiculos();
-        new Coche(ID,DESCRIPCION);
+        coche = new Coche(ID,DESCRIPCION, new CategoriaA());
     }
 
     @Test
@@ -30,27 +30,28 @@ public class GestorVehiculosTest {
     }
     @Test
     public void testInsertar() {
-        assertEquals(null, gestorVehiculos.buscar(coche));
+        gestorVehiculos.eliminar(coche);
+        assertEquals(null, gestorVehiculos.buscar(coche.getId()));
         gestorVehiculos.insertar(coche);
-        assertEquals(gestorVehiculos.buscar(coche), coche);
+        assertEquals(gestorVehiculos.buscar(coche.getId()).getId(), coche.getId());
     }
     @Test
     public void testBuscar() {
         gestorVehiculos.insertar(coche);
-        assertEquals(gestorVehiculos.buscar(coche), coche);
+        assertEquals(gestorVehiculos.buscar(coche.getId()).getId(), coche.getId());
     }
     @Test
     public void testActualizar() {
-        String nuevaDescripcion = coche.getDescripcion()+ "110 CV";
+        String nuevaDescripcion = coche.descripcion+ "110 CV";
         coche.setDescripcion(nuevaDescripcion);    
         gestorVehiculos.actualizar(coche);
-        assertEquals(gestorVehiculos.buscar(coche).getDescripcion(), nuevaDescripcion);
-        assertEquals(gestorVehiculos.buscar(coche).getId(), coche.getId());
-        assertEquals(gestorVehiculos.buscar(coche), coche);
+        assertEquals(gestorVehiculos.buscar(coche.getId()).getDescripcion(), nuevaDescripcion);
+        assertEquals(gestorVehiculos.buscar(coche.getId()).getId(), coche.getId());
+        assertEquals(gestorVehiculos.buscar(coche.getId()).getId(), coche.getId());
     }
     @Test
     public void testEliminar() {
         gestorVehiculos.eliminar(coche);
-        assertEquals(gestorVehiculos.buscar(coche), null);
+        assertEquals(gestorVehiculos.buscar(coche.getId()), null);
     }
 }
